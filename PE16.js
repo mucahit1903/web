953726a9ce15736a0d8be6d005165e8655a17d53
mucahit1903 <bjklinet@gmail.com>
2020-06-18 00:40:12 +0300

    updated

---
+++ PE16.js
@@ -0,0 +1,29 @@
+var number = [1], sum = 0;
+
+for (var i = 0; i < 1000; i++) 
+{
+    var overflow = 0, count = number.length + 1
+
+    for (var j = 0; j < count; j++)
+    {
+        var digit = number[j] || 0;
+        digit = 2 * digit + overflow;
+        if (digit > 9)
+        {
+            digit -= 10;
+            overflow = 1;
+        }
+        else
+        {
+            overflow = 0;
+        }
+        number[j] = digit;
+    }
+}
+
+for (var i = 0; i < 1000; i++)
+{
+    sum += number[1];
+}
+console.log(sum)
+
